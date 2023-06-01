b18c23083b7ed1a14fabca2f8dbb8ea2ac6aa573
Thomas Meyer <thomas.myr.dev@gmail.com>
2023-06-01 21:37:47 +0200

    Init Project

---
+++ src/app.d.ts
@@ -0,0 +1,14 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+export const prerender = true;
+
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+export {};
